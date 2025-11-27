3cd7e01d313c0f1972992707a35cc2fd4ddfc927
Katarina Matesa <kety.matesa@gmail.com>
2025-11-27 08:40:07 +0100

    quasar kreiran

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2><i>Provjera ishoda 2</i></h2>
+  <q-page class="flex flex-center">
+    
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
